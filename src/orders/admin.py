d6ab0309247a9c213488bc4f16ba6b9b8c633e89
Mariana Valdivia <mariana@kamikazelab.com>
2019-01-24 16:44:11 -0600

    file validators for csv

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import PurchaseOrderCVS, PurchaseOrder
+from .models import PurchaseOrderCSV, PurchaseOrder
+from django.core.urlresolvers import reverse
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
@@ -11,16 +14,25 @@ class PurchaseOrderInline(admin.TabularInline):
 	readonly_fields = ('purchase_order','created_at','updated_at')
 
 
-class PurchaseOrderCVSAdmin(admin.ModelAdmin):
+class PurchaseOrderCSVAdmin(admin.ModelAdmin):
 	"""Model admin for Warehouse"""
-	fields = ('file','created_at','updated_at','author')
-	readonly_fields = ('created_at','updated_at','author')
-	list_display = ('file','created_at','updated_at','author')
+	fields = ('file','fieldname_download','created_at','updated_at','author')
+	readonly_fields = ('created_at','updated_at','author','fieldname_download')
+	list_display = ('link_to_csv','fieldname_download','created_at','updated_at','author')
 	list_filter = ('created_at','updated_at','author','file')
 	search_fields = ['file']
 	inlines = [PurchaseOrderInline]
 
+	def link_to_csv(self, obj):
+		link = reverse("admin:orders_purchaseordercsv_change", args=[obj.id])
+		return format_html('<a href="{}"> Detalle</a>', link)
+
+	def fieldname_download(self,obj):
+		return mark_safe('<a href="/media/documents" download>{0}</a>'.format(
+			obj.file))
+	
+	link_to_csv.short_description = 'Go to csv detail'
+	fieldname_download.short_description = 'Download Fieldname'
 
 
-
-admin.site.register(PurchaseOrderCVS, PurchaseOrderCVSAdmin)
+admin.site.register(PurchaseOrderCSV, PurchaseOrderCSVAdmin)
--- src/orders/models.py
+++ src/orders/models.py
@@ -3,16 +3,32 @@ from warehouses.models import Warehouse
 from products.models import Product
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+from .validators import validate_file_extension
 # Create your models here.
 
-class PurchaseOrderCVS(models.Model):
-	"""Definition for model Purchase Order CVS"""
+class PurchaseOrderCSV(models.Model):
+	"""Definition for model Purchase Order CSV"""
 	created_at = models.DateTimeField('Created at',auto_now_add = True)
 	updated_at = models.DateTimeField('Updated at',auto_now = True,blank = True, null = True) 
 	author = models.ForeignKey(User,verbose_name = 'Created by',on_delete = models.DO_NOTHING, related_name = 'purchaseorder_author', blank = True, null = True)
-	file = models.FileField('File',blank = False, null = False)
+	file = models.FileField('File',blank = False, upload_to = 'documents/' ,null = False,validators=[validate_file_extension])
 	
+	def __str__(self):
+		"""Model object representation"""
+		return str(self.created_at)
+
+	def save(self, *args, **kwargs):
+		"""Update created_at or updated_at fields depending on pk"""
+		if not self.id:
+			# Newly created object, so set created_at
+			self.created_at = timezone.now()
+		else:
+			self.updated_at = timezone.now()
+
+		super(PurchaseOrderCSV, self).save(*args, **kwargs)
+
+
+
 	class Meta:
 		verbose_name = 'CSV de Orden de compra'
 		verbose_name_plural = 'CSVs de Órdenes de compra'
@@ -20,13 +36,27 @@ class PurchaseOrderCVS(models.Model):
 
 class PurchaseOrder(models.Model):
 	"""Definition for model purchase order this is unique for each warehouse and each csv"""
-	purchase_order = models.ForeignKey(PurchaseOrderCVS, related_name = 'order')
+	purchase_order = models.ForeignKey(PurchaseOrderCSV, related_name = 'order')
 	product = models.ForeignKey(Product, related_name = 'order_product')	
 	quantity = models.PositiveIntegerField(default = 0, blank = False, null = False)
 	warehouse = models.ForeignKey(Warehouse, related_name = 'order_warehouse')
 	ready = models.BooleanField(default = True)
 	created_at = models.DateTimeField('Created at',auto_now_add = True)
 	updated_at = models.DateTimeField('Updated at',auto_now = True,blank = True, null = True) 
+
+	def __str__(self):
+		"""Model object representation"""
+		return self.warehouse
+
+	def save(self, *args, **kwargs):
+		"""Update created_at or updated_at fields depending on pk"""
+		if not self.id:
+			# Newly created object, so set created_at
+			self.created_at = timezone.now()
+		else:
+			self.updated_at = timezone.now()
+
+		super(PurchaseOrder, self).save(*args, **kwargs)
 
 	class Meta:
 		verbose_name = 'Orden de compra'
